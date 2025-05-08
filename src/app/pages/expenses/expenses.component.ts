3b2d6662beacc8d2f2e6151b5e84823d5b31b2d3
abbes manel <you@example.com>
2025-05-08 09:38:50 +0100

    add-code

--- src/app/pages/expenses/expenses.component.ts
+++ src/app/pages/expenses/expenses.component.ts
@@ -3,20 +3,21 @@ import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ExpenseService } from '../../services/expense.service';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   standalone: true,
   selector: 'app-expenses',
-  imports: [MatTableModule, MatButtonModule, MatIconModule],
+  imports: [MatTableModule, MatButtonModule, MatIconModule,MatDialogModule],
   templateUrl: './expenses.component.html',
   styleUrls: ['./expenses.component.css']
 })
 export class ExpensesComponent {
   constructor(
     private expenseService: ExpenseService,
-    
+    private dialog: MatDialog
   ) {}
-  displayedColumns: string[] = ['date', 'category', 'description','amount','actions'];
+  displayedColumns: string[] = ['date', 'category', 'description','amount','status','actions'];
 
   dataSource = [
    
@@ -36,6 +37,19 @@ ngOnInit(){
     // You can add logic here to open a dialog or inline form for editing
   }
 
+  // openAddExpenseDialog(): void {
+  //   const dialogRef = this.dialog.open(AddExpenseDialogComponent, {
+  //     width: '400px'
+  //   });
+  
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     if (result) {
+  //       // You can call your API to save `result`
+  //       console.log('New Expense:', result);
+  //       // e.g., this.expenseService.addExpense(result).subscribe(...)
+  //     }
+  //   });
+  // }
   deleteItem(element: any) {
     // console.log('Delete:', element);
     // this.dataSource = this.dataSource.filter(item => item.id !== element.id);
